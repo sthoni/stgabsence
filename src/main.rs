12d9d345237bc8262b840cc0b6f4be22822d7967
Sebastian Thon <sthon@hotmail.de>
2023-10-26 23:40:21 +0200

    Alle Zeiten werden nun korrekt berechnet.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use time::macros::format_description;
+use time::Date;
 use time::Duration;
 use time::Time;
 //tutorial-read-01.rs
@@ -16,9 +17,12 @@ struct Record {
     aktualisiert: String,
 }
 
+const SCHOOL_HOUR_IN_MINUTES: i64 = 67;
+const SCHOOL_DAY_IN_MINUTES: i64 = SCHOOL_HOUR_IN_MINUTES * 4;
+
 fn run() -> Result<(), Box<dyn Error>> {
     let abwesenheit_time_format = format_description!("[hour]:[minute]");
-    let abwesenheit_date_format = format_description!("[day]");
+    let abwesenheit_date_format = format_description!("[day]:[month]:[year]");
 
     let file_path = get_first_arg()?;
     let file = File::open(file_path)?;
@@ -36,14 +40,18 @@ fn run() -> Result<(), Box<dyn Error>> {
                 let duration: Duration = end_zeit - start_zeit;
                 record.abwesenheitszeit = duration.whole_minutes().to_string();
             } else {
-                println!("{:?}", &record.abwesenheitszeit[0..10]);
-                let start_datum = Time::parse("01", &abwesenheit_date_format).unwrap();
-                let end_datum = Time::parse("15", &abwesenheit_date_format).unwrap();
+                let start_datum =
+                    Date::parse(&record.abwesenheitszeit[0..10], &abwesenheit_date_format).unwrap();
+                let end_datum =
+                    Date::parse(&record.abwesenheitszeit[13..23], &abwesenheit_date_format)
+                        .unwrap();
                 let duration: Duration = end_datum - start_datum;
-                record.abwesenheitszeit = duration.whole_days().to_string();
+                let duration_in_days = duration.whole_days() + 1;
+                let duration_in_hours = duration_in_days * 4 * SCHOOL_HOUR_IN_MINUTES;
+                record.abwesenheitszeit = duration_in_hours.to_string();
             }
         } else {
-            record.abwesenheitszeit = String::from("268");
+            record.abwesenheitszeit = SCHOOL_DAY_IN_MINUTES.to_string();
         }
         println!("{:?}", record);
     }
